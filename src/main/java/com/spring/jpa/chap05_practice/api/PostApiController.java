dd3a35815821bd9b3a9196685fce0aecb4b75df6
kyoungmin <stephen4951@gmail.com>
2023-06-20 12:59:45 +0900

    # 9. 종합 연습예제 검증 완료 (INSERT)

--- src/main/java/com/spring/jpa/chap05_practice/api/PostApiController.java
+++ src/main/java/com/spring/jpa/chap05_practice/api/PostApiController.java
@@ -81,9 +81,18 @@ public class PostApiController {
                     .body(fieldErrors);
         }
 
-        PostDetailResponseDTO responseDTO = postService.insert(dto);
+        try {
+            PostDetailResponseDTO responseDTO = postService.insert(dto);
+            return ResponseEntity
+                    .ok()
+                    .body(responseDTO);
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            return ResponseEntity
+                    .internalServerError()
+                    .body("미안 서버 터졌어... 원인: " + e.getMessage());
+        }
 
-        return null;
     }
 
 
